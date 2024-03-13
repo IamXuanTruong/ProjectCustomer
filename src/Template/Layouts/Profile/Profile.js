5850f67cd20de463ba54a183a22cbb171f344997
IamXuanTruong <iamxuantruongg@gmail.com>
2024-03-13 14:12:41 +0700

    update

--- src/Template/Layouts/Profile/Profile.js
+++ src/Template/Layouts/Profile/Profile.js
@@ -21,10 +21,10 @@ function Profile() {
                         <p>Tel: +84398946681</p>
                     </div>
                     <div className='media_profile'>
-                        <a href=''>
+                        <a href='https://t.me/+84398946681?fbclid=IwAR1E91WKzzBn3b2J2fcjI0-wqFA2ROIVQ2ul0zIBU-rtlu6iFXn9UrJBoRI'>
                             <i class="fa-brands fa-telegram"></i>
                         </a>
-                        <a href=''>
+                        <a href='https://join.skype.com/invite/YFBIFuufDM6F?fbclid=IwAR2um0Do8TM-tG54B0_-qQgPtCmnZPyesMo-SU0XT1u3tryu8AA3FjzlUHw'>
                             <i class="fa-brands fa-skype"></i>
                         </a>
                         <a href='https://www.facebook.com/giovohinh.nguoivotinh.9'>
